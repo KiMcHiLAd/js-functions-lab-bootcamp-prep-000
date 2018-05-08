7e166d81daaa63f5c286a1cd5a903290a9fbf90d
Hania Bigo <hrbigos@umich.edu>
2018-05-08 19:06:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function happyHolidays(string) {
 }
 
 function happyHolidaysTo(name) {
-  return (`Happy holidays, ${name}!`)
+  return(`Happy holidays, ${name}!`)
 }
 
+function happyHolidayTo(holiday, name) {
+ return(`Happy ${holiday}, ${name}!`)
+}
 
